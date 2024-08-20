7d8271b29066447a60e2a5533d78630cbeaad307
kiu_shubitidze.sandro <shubitidze.sandro@kiu.edu.ge>
2024-08-20 12:56:45 +0400

    Add vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import MainComponent from './MainComponent';
+import { Analytics } from "@vercel/analytics/react"
 import './App.css';
+
 
 
 function App() {
   return (
     <div className="App">
       <MainComponent/>
+      <Analytics />
     </div>
   );
 }
